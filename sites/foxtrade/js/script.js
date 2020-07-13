bc5b475fb34f12f530ae18657c32f25706ea750d
viktorenko <vityablokhin7@gmail.com>
2020-07-13 18:26:08 +0300

    as

--- sites/foxtrade/js/script.js
+++ sites/foxtrade/js/script.js
@@ -41,4 +41,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     new DB().getData().then(renderCard);
 
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('nav');
+
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('active');
+        nav.classList.toggle('show');
+    });
+
 });
