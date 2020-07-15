0332b885b83784b592963f3c2a453a847dc4d814
viktorenko <vityablokhin7@gmail.com>
2020-07-15 19:20:56 +0300

    404 done

--- sites/foxtrade/js/script.js
+++ sites/foxtrade/js/script.js
@@ -1,45 +1,45 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
 
-    const main = document.querySelector('main'),
-        url = 'db/db.json';
+    // const main = document.querySelector('main'),
+    //     url = 'db/db.json';
 
-    class DB {
-        getData = async () => {
-            const res = await fetch(url);
-            if (res.ok) {
-                return res.json();
-            } else {
-                throw new Error(`Не удалось получить данные по адресу ${url}`);
-            }
-        }
+    // class DB {
+    //     getData = async () => {
+    //         const res = await fetch(url);
+    //         if (res.ok) {
+    //             return res.json();
+    //         } else {
+    //             throw new Error(`Не удалось получить данные по адресу ${url}`);
+    //         }
+    //     }
 
-    };
+    // };
 
-    const renderCard = response => {
-        main.textContent = '';
+    // const renderCard = response => {
+    //     main.textContent = '';
 
-        response.forEach(item => {
+    //     response.forEach(item => {
 
-            const { img, title, subTitle } = item;
+    //         const { img, title, subTitle } = item;
 
-            const card = document.createElement('div');
-            card.className = 'post';
-            card.innerHTML = `
-                <div class="carousel">
-                    <img src="db/img/${img}.jpg" alt="${title}" onerror="this.src='db/img/default.jpg'" />
-                </div>
-                <i class="fab fa-bitcoin"></i>
-                <h3>${title}</h3>
-                <p>${subTitle}</p>
-                <a href=""><i class="fa fa-sign-in-alt"></i> Узнать больше</a>
-            `;
+    //         const card = document.createElement('div');
+    //         card.className = 'post';
+    //         card.innerHTML = `
+    //             <div class="carousel">
+    //                 <img src="db/img/${img}.jpg" alt="${title}" onerror="this.src='db/img/default.jpg'" />
+    //             </div>
+    //             <i class="fab fa-bitcoin"></i>
+    //             <h3>${title}</h3>
+    //             <p>${subTitle}</p>
+    //             <a href=""><i class="fa fa-sign-in-alt"></i> Узнать больше</a>
+    //         `;
 
-            main.append(card);
-        });
-    };
+    //         main.append(card);
+    //     });
+    // };
 
-    new DB().getData().then(renderCard);
+    // new DB().getData().then(renderCard);
 
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('nav');
